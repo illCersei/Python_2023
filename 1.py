259fb7cf7f654f75af38bbaa8047a585715703d8
Cersei <bestkarmarussia@gmail.com>
2023-09-09 23:10:41 +0300

    0

--- 1.py
+++ 1.py
@@ -2,16 +2,16 @@ import random
 import re
 
 number_str = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
-print(number_str)
-success = 0
+#print(number_str)
+success = bool(0)
 
-while success == 0:
-    valid_test = 0
-    while valid_test == 0:
+while success == False:
+    valid_test = bool(0)
+    while valid_test == False:
         print("Введите 4ёх значое число:")
         player_number_str = str(input())
         if re.fullmatch('\d\d\d\d', player_number_str):
-            valid_test = 1
+            valid_test = True
         else:
             print("Ввод некорректен")
 
@@ -22,12 +22,12 @@ while success == 0:
         if (player_number_str[x] in number_str) and (player_number_str[x] not in temp) :
             temp.append(player_number_str[x])
             korova+=number_str.count(player_number_str[x])
-            print("Коров найдено: ", number_str.count(player_number_str[x]))
+            #print("Коров найдено: ", number_str.count(player_number_str[x]))
         if number_str[x] == player_number_str[x]:
             bik+=1
 
     print("Быков", bik)
     print("Коров", korova)
     if bik == 4:
-        success = 1
+        success = True
         print("Победа")
