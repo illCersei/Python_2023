e513edb9351127d5752e09a82c668d69f11ec8e9
Cersei <bestkarmarussia@gmail.com>
2023-09-17 16:27:58 +0300

    2 done

--- 2.py
+++ 2.py
@@ -1,8 +1,9 @@
 def start_game():
     global string0, string1,string2
-    string0 = [""] * 7
-    string1 = [""] * 7
-    string2 = [""] * 7
+    string0 = [" "] * 3
+    string1 = [" "] * 3
+    string2 = [" "] * 3
+
     print("Игра крестики и нолики на 3х3 поле." + '\n' +
     "Вводить нужно координаты клетки, куда вы ставите свой знак." + '\n' +
     "Первая координата - строка, вторая - столбец." + '\n' +
@@ -10,56 +11,68 @@ def start_game():
         
 def print_pole():
     print("Ситуация на поле:")
-    print(" ".join(string0)+"\n"+" ".join(string1)+"\n"+" ".join(string2))
-
+    print(" _____")
+    print("|" +" ".join(string0)+ "|" + "\n"+ "|" +" ".join(string1)+ "|" + "\n" + "|" + " ".join(string2)+ "|")
+    print(" ‾‾‾‾‾")
 
 def win_check(counter):
+    if counter == 8:
+        print("Ничья")
+        return True
+        
     temp = ("".join(string0) + "".join(string1) + "".join(string2))
     win = bool(0)
     if "OOO" in temp or "XXX" in temp:
         win = True
-    for i in range(3):
-        if string0[i] == string1[i] == string2[i]:
-            win = True
-    if string0[0] == string1[1] == string2[2] or string0[3] == string1[1] == string2[0]:
+    elif string0[0] == string1[1] == string2[2] == 'X' or string0[0] == string1[1] == string2[2] == 'O' or string0[2] == string1[1] == string2[0] == 'X' or string0[2] == string1[1] == string2[0] == 'O':
         win = True
+    else:
+        for i in range(3):
+            if string0[i] == string1[i] == string2[i] == 'X' or string0[i] == string1[i] == string2[i] == 'O':
+                win = True
     if win == True:
         if counter%2 == 0:
             print("Победил первый игрок(Х)")
         else:
-            print("Победил второй игрок(0)")
+            print("Победил второй игрок(O)")
         return True
+    
     else:
         return False
-
-                
+      
 def player_turn(s,x,y):
     match x:
         case 0:
-            if string0[y] == "":
+            if string0[y] == " ":
                 string0[y] = s
                 return True
         case 1:
-            if string1[y] == "":
+            if string1[y] == " ":
                 string1[y] = s
                 return True
         case 2:
-            if string2[y] == "":
+            if string2[y] == " ":
                 string2[y] = s  
                 return True 
         case _:
             return False
         
-def player_input(q):
+def player_input(s,q):
+    match s:
+        case 'X':
+            print("Ходит первый игрок(Х)")
+        case _:
+            print("Ходит второй игрок(О)")
     valid_enter = bool(0)
     while valid_enter != True:
         if q == 0:
             print("Введите номер строки ")
         else:
             print("Введите номер столбца ")
-        n = int(input())
-        if n == 1 or n == 2 or n == 3:
+        temp = input()
+        if temp == '1' or temp == '2' or temp == '3':
             valid_enter = True
+            n = int(temp)
         else:
             print("Ошибка ввода")
     
@@ -67,24 +80,23 @@ def player_input(q):
         
 start_game()        
 counter = 0
-while(win_check(counter) == False):
+while(win_check(counter-1) == False):
     check_movement = True
     if counter%2 == 0:
         s = 'X'
     else:
         s = 'O'
 
-    x = player_input(0)
-    y = player_input(1)
+    x = player_input(s,0)
+    y = player_input(s,1)
 
     check_movement = player_turn(s,x,y)
 
     if check_movement == True:
-        print_pole()
         counter +=1
     else:
         print("Клетка уже занята, повторите ввод")
-        print_pole()
+    print_pole()
 
                  
 
